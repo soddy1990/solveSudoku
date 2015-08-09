b5cdca12ca6d3dd30f12e2449835e40a159c713d
soddy1990 <soddy1990@gmail.com>
2015-08-08 23:40:21 -0400

    AC

--- SudokuSolver/SudokuSolver.cpp
+++ SudokuSolver/SudokuSolver.cpp
@@ -3,74 +3,6 @@
 #include <iostream>
 using namespace std;
 
-bool groupcheck(vector<vector<char>>& board) {
-	for(int i = 0; i < board.size(); i+=3){
-		for(int j = 0; j < board[i].size(); j+=3){
-			set<int> checkboard;
-			int count = 0;
-			for(int k = 0; k<3; k++){
-				for(int l = 0; l<3; l++){
-					if(board[i+k][j+l] != '.'){
-						count++;
-						checkboard.insert(board[i+k][j+l]);
-					}
-				}
-			}
-			if(count != checkboard.size()){
-				return false;
-			}
-		}
-	}
-	return true;
-}
-
-bool rowcheck(vector<vector<char>>& board) {
-	for(int i = 0; i < board.size(); i++){
-		set<int> checkboard;
-		int count = 0;
-		for(int j = 0; j < board[i].size(); j++){
-			if(board[i][j] != '.'){
-				count++;
-				checkboard.insert(board[i][j]);
-			}
-		}
-		if(count != checkboard.size()){
-			return false;
-		}
-	}
-	return true;
-}
-
-bool columncheck(vector<vector<char>>& board) {
-	for(int i = 0; i < board[0].size(); i++){
-		set<int> checkboard;
-		int count = 0;
-		for(int j = 0; j < board.size(); j++){
-			if(board[j][i] != '.'){
-				count++;
-				checkboard.insert(board[j][i]);
-			}
-		}
-		if(count != checkboard.size()){
-			return false;
-		}
-	}
-	return true;
-}
-
-bool isValidSudoku(vector<vector<char>>& board) {
-	if(board.size()<3){
-		return false;
-	}
-	for(int i = 0; i<board.size(); i++){
-		if(board[i].size() != board.size())
-			return false;
-	}
-	if(groupcheck(board) && rowcheck(board) && columncheck(board)){
-		return true;
-	}
-	return false;
-}
 
 void show(vector<vector<char>>& board){
 	for(int i = 0; i < board[0].size(); i++){
@@ -81,110 +13,106 @@ void show(vector<vector<char>>& board){
 	}
 }
 
-bool solver(vector<vector<char>>& board, int i, int j, vector<vector<vector<int>>> &available){
-
-	for(int k = 0; k<available[i][j].size(); k++){
-		board[i][j] = available[i][j][k] + 48;
-		//show(board);
-		//cout<<endl;
-		bool mark = false;
-		if(isValidSudoku(board)){
-			int row = i;
-			int col = j;
-			while(row < board.size()){
-				while(col < board[row].size()){
-					if(board[row][col] != '.'){
-						col++;
-					}
-					else{
-						break;
-					}
-				}
-				if(col == board[row].size()){
-					if(row+1<board.size()){
-						row++;
-						col = 0;
-						continue;
-					}
-					return true;
-				}else{
-					if(!solver(board, row, col, available)){
-						mark = true;
-						break;
-					}
-				}
-			}
-			if(mark){
-				continue;
+bool groupcheck(vector<vector<char>>& board, int i, int j) {
+	int qu_i = i/3;
+	int qu_j = j/3;
+	set<int> checkboard;
+	int count = 0;
+	for(int p = 0; p<3; p++){
+		for(int q =0; q<3; q++){
+			if(board[3*qu_i+p][3*qu_j+q] != '.'){
+				count++;
+				checkboard.insert(board[3*qu_i+p][3*qu_j+q]);
 			}
 		}
 	}
-	board[i][j] = '.';
-	//show(board);
-	//cout<<endl;
-	return false;
+	if(count != checkboard.size()){
+		return false;
+	}
+	return true;
+}
+bool rowcheck(vector<vector<char>>& board, int j) {
+	set<int> checkboard;
+	int count = 0;
+	for(int k = 0; k < board.size(); k++){
+		if(board[k][j] != '.'){
+			count++;
+			checkboard.insert(board[k][j]);
+		}
+	}
+	if(count != checkboard.size()){
+		return false;
+	}
+	return true;
+}
+
+bool columncheck(vector<vector<char>>& board, int i) {
+	set<int> checkboard;
+	int count = 0;
+	for(int k = 0; k < board.size(); k++){
+		if(board[i][k] != '.'){
+			count++;
+			checkboard.insert(board[i][k]);
+		}
+	}
+	if(count != checkboard.size()){
+		return false;
+	}
+	return true;
+}
+
+bool solver(vector<vector<char>>& board, int i, int j, int n){
+	if(j == n){
+		j = 0;
+		i++;
+	}
+	if(i == n){
+		return true;
+	} 
+	if(board[i][j] != '.'){
+		if(j<n)
+			return solver(board, i, j+1, n);
+		else
+			return solver(board, i+1, 0, n);
+	}else{
+		for(int k = 1; k<=9; k++){
+			board[i][j] = k+48;
+			//cout<<endl;
+			//show(board);
+			if(groupcheck(board,i,j) && rowcheck(board, j) && columncheck(board, i)){
+				if(j<n){
+					if(solver(board, i, j+1, n)){
+						return true;
+					}
+					else{
+						board[i][j] = '.';
+						continue;
+					}
+				}
+				else{
+					if(solver(board, i+1, j, n)){
+						return true;
+					}
+					else{
+						board[i][j] = '.';
+						continue;
+					}
+				}
+			}
+			board[i][j] = '.';
+		}
+		return false;
+	}
 }
 
 void solveSudoku(vector<vector<char>>& board) {
-	int row = 0;
-	int col = 0;
-
-
-	vector<vector<vector<int>>> available; 
-	for(int i = 0; i<board.size(); i++){
-		vector<vector<int>> available_1;
-		for(int j = 0; j<board[i].size(); j++){
-			vector<int> available_2;
-			bool available[9];
-			for(int a_tab = 0; a_tab<9; a_tab++){
-				available[a_tab] = true;
-			}
-			for(int a_tab = 0; a_tab< board.size(); a_tab++){
-				if(board[a_tab][j] != '.'){
-					available[board[a_tab][j] - 48 - 1] = false;
-				}
-			}
-			for(int a_tab = 0; a_tab< board[i].size(); a_tab++){
-				if(board[i][a_tab] != '.'){
-					available[board[i][a_tab] - 48 - 1] = false;
-				}
-			}
-			int group_i = i/3;
-			int group_j = j/3;
-			for(int k = 0; k<3; k++){
-				for(int l = 0; l<3; l++){
-					if(board[group_i*3 + k][group_j*3 + l] != '.'){
-						available[board[group_i*3 + k][group_j*3 + l] - 48 - 1] = false;
-					}
-				}
-			}
-			for(int a_tab = 0; a_tab < 9; a_tab++){
-				if(available[a_tab]){
-					available_2.push_back(a_tab+1);
-				}
-			}
-			available_1.push_back(available_2);
-		}
-		available.push_back(available_1);
-	}
-
-	while(row < board.size()){
-		while(col < board[row].size()){
-			if(board[row][col] != '.'){
-				col++;
-			}
-			else{
-				break;
-			}
-		}
-		if(board[row][col] == '.'){
-			solver(board, row, col, available);
-		}else{
-			if(row+1<board.size()){
-				row++;
-				col = 0;
-				continue;
-			}
+	int rows = board.size();
+	if(rows>0){
+		int cols = board[0].size();
+		int n = 0;
+		if(cols == rows){
+			n = rows;
+			solver(board, 0, 0, n);
 		}
 	}
 }
@@ -408,7 +336,9 @@ void main(){
 	line.push_back('9');
 	board.push_back(line);
 	line.clear();*/
-
+		
+	show(board);
+	cout<<endl;
 	solveSudoku(board);
 	//cout<<isValidSudoku(board);
 	show(board);
